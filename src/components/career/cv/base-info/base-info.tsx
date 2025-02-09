9d9c0b7913f4acf32e5388ca9b5975a17a55d831
Evgenii Perminov <perminov.eugene@gmail.com>
2025-02-09 12:53:25 +0200

    Fix links

--- src/components/career/cv/base-info/base-info.tsx
+++ src/components/career/cv/base-info/base-info.tsx
@@ -15,11 +15,11 @@ const BaseInfo = () => {
       <div className="border-t border-b border-gray-300 border-t-4 border-b-4 py-2 p-4 mb-1 flex justify-center flex-col md:flex-row ">
         <div className="text-sm mr-8">
           <ContactGit address="https://github.com/PerminovEugene" />
-          <ContactLinkedin address="https://linkedin.com/in/Evgenii-perminov" />
+          <ContactLinkedin address="https://linkedin.com/in/Eugene-Perminov" />
         </div>
         <div className="text-sm align-right">
           <ContactPhone phone="+372-5368-5244" />
-          <ContactEmail address="perminov.Evgenii@gmail.com" />
+          <ContactEmail address="perminov.eugene@gmail.com" />
         </div>
       </div>
     </div>
